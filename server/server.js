4c9d6311025f6cc8c16f36847dfa4803951826a6
DawidKlejc <klejcd@gmail.com>
2022-05-09 02:19:32 -0400

    Updated env variables

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const app = express();
 require('dotenv').config();
-let port = process.env.HOST_PORT;
+let port = process.env.HOST_PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
