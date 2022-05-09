bae12c0d3fb0ddfa029c7bb71354f8eb15bdb86c
DawidKlejc <klejcd@gmail.com>
2022-05-09 16:19:40 -0400

    updates

--- server/db/dbconfig.js
+++ server/db/dbconfig.js
@@ -1,9 +1,8 @@
 require('dotenv').config();
 
 module.exports = {
-    host: process.env.HOST,
-    user: process.env.USER,
-    password: process.env.PASSWORD,
-    database: process.env.DATABASE,
-    port: process.env.PORT
+    host: process.env.LOCAL_HOST,
+    user: process.env.LOCAL_USER,
+    password: process.env.LOCAL_PASSWORD,
+    database: process.env.LOCAL_DATABASE,
 }
--- server/db/index.js
+++ server/db/index.js
@@ -1,8 +1,20 @@
 const mysql = require('mysql');
 const dbInfo = require('./dbconfig');
 
+
+let connection;
+
+let checkConnection = () => {
+    if(process.env.CLEARDB_DATABASE_URL) {
+        connection = mysql.createConnection(process.env.CLEARDB_DATABASE_URL);
+    } else {
+        connection = mysql.createConnection(dbInfo);
+    }
+}
+
 const getISQData = new Promise((resolve, reject) => {
-    const connection = mysql.createConnection(dbInfo);
+    // const connection = mysql.createConnection(dbInfo);
+    checkConnection();
     connection.connect();
     connection.query(`SELECT * FROM ISQ_DATA`, (err, result) => {
         connection.end();
@@ -12,7 +24,8 @@ const getISQData = new Promise((resolve, reject) => {
 
 const getRating = (courseName, professorsLastName) => {
     return new Promise((resolve, reject) => {
-        const connection = mysql.createConnection(dbInfo);
+        // const connection = mysql.createConnection(dbInfo);
+        checkConnection();
         connection.connect();
         connection.query(`SELECT Rating FROM ISQ_DATA WHERE Course = ? AND ProfessorName = ?`, [courseName, professorsLastName], (err, result) => {
             connection.end();
