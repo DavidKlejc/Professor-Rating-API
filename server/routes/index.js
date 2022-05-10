652b53af15324090b3ea2a185d3c7b7334632bb8
DawidKlejc <klejcd@gmail.com>
2022-05-10 19:36:37 -0400

    Fixed routing issue preventing api requests

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { getISQData, getRating } = require('../db');
 const router = express.Router();
 
-router.get('/ratings', (req, res) => {
+router.get('/ratings', (req, res, next) => {
     try {
         getISQData.then((result) => {
             res.json(result);
@@ -10,6 +10,7 @@ router.get('/ratings', (req, res) => {
     } catch(error) {
         throw error;
     }
+    next();
 });
 
 router.get('/:course/:professorLastName', (req, res) => {
