7268dfcfd5158eabfae624dafadc0de444812241
DawidKlejc <klejcd@gmail.com>
2022-05-10 23:20:43 -0400

    updated server

--- server/server.js
+++ server/server.js
@@ -1,21 +1,14 @@
 const express = require('express');
+const cors = require('cors');
 const apiRouter = require('./routes');
 const bodyParser = require('body-parser');
-const cors = require('cors');
-const corsProxy = require('cors-anywhere');
 const app = express();
 require('dotenv').config();
 let port = process.env.PORT || 3000;  
 
-corsProxy.createServer({
-    originWhitelist: ['https://banregister.unf.edu/StudentRegistrationSsb/ssb/classSearch/classSearch', 'https://banregister.unf.edu/StudentRegistrationSsb/ssb/classSearch/classSearch/*'],
-    requireHeader: ['origin', 'x-requested-with'],
-    removeHeaders: ['cookie', 'cookie2']
-})
-
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(cors());
 
 app.use('/', apiRouter);
 
