6bdda70f3e6070f62444fa37e8c2e4f865105724
DawidKlejc <klejcd@gmail.com>
2022-05-10 23:05:51 -0400

    added CORS anywhere

--- server/server.js
+++ server/server.js
@@ -2,9 +2,16 @@ const express = require('express');
 const apiRouter = require('./routes');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const corsProxy = require('cors-anywhere');
 const app = express();
 require('dotenv').config();
 let port = process.env.PORT || 3000;  
+
+corsProxy.createServer({
+    originWhitelist: ['https://banregister.unf.edu/StudentRegistrationSsb/ssb/classSearch/classSearch', 'https://banregister.unf.edu/StudentRegistrationSsb/ssb/classSearch/classSearch/*'],
+    requireHeader: ['origin', 'x-requested-with'],
+    removeHeaders: ['cookie', 'cookie2']
+})
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
