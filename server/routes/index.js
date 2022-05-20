e2fec3a203b7eee5e6ce60b5a13d90c4d45d28fe
DawidKlejc <klejcd@gmail.com>
2022-05-20 19:14:30 -0400

    fixing server crash

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const { getISQData, getRating } = require('../db');
 const router = express.Router();
+const corsMiddleware = require('./cors');
 
-router.get('/ratings', (req, res) => {
+router.get('/ratings', corsMiddleware(corsOptions), (req, res) => {
     try {
         getISQData.then((result) => {
             res.json(result);
@@ -12,7 +13,7 @@ router.get('/ratings', (req, res) => {
     }
 });
 
-router.get('/:course/:professorLastName', (req, res) => {
+router.get('/:course/:professorLastName', corsMiddleware(corsOptions), (req, res) => {
     try {
         getRating(req.params.course, req.params.professorLastName).then((result) => {
             res.json(result);
