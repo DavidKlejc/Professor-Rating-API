9bf9a61186a55fe0e5fda30b1b09e7fb5018a873
DawidKlejc <klejcd@gmail.com>
2022-05-21 04:23:58 -0400

    refactored sql connection

--- server/db/index.js
+++ server/db/index.js
@@ -3,33 +3,44 @@ const dbInfo = require('./dbconfig');
 
 
 let connection;
-
-let checkConnection = () => {
+let createPool = () => {
     if(process.env.CLEARDB_DATABASE_URL) {
-        connection = mysql.createConnection(process.env.CLEARDB_DATABASE_URL);
+        connection = mysql.createPool(process.env.CLEARDB_DATABASE_URL);
     } else {
-        connection = mysql.createConnection(dbInfo);
+        connection = mysql.createPool(dbInfo);
     }
-    connection.connect();
-}
+};
 
-checkConnection();
+createPool();
 
 const getISQData = new Promise((resolve, reject) => {
-    connection.query(`SELECT * FROM ISQ_DATA`, (err, result) => {
-        return err ? reject(err) : resolve(result);
+    
+    connection.getConnection((err, connection) => {
+        if(err) {
+            connection.release();
+            throw err;
+        }
+        connection.query(`SELECT * FROM ISQ_DATA`, (err, result) => {
+            connection.release();
+            return err ? reject(err) : resolve(result);
+        });
     });
 }); 
 
 const getRating = (courseName, professorsLastName) => {
     return new Promise((resolve, reject) => {
-        connection.query(`SELECT Rating FROM ISQ_DATA WHERE Course = ? AND ProfessorName = ?`, [courseName, professorsLastName], (err, result) => {
-            return err ? reject(err) : resolve(result);
+        connection.getConnection((err, connection) => {
+            if(err) {
+                connection.release();
+                throw err;
+            }
+            connection.query(`SELECT Rating FROM ISQ_DATA WHERE Course = ? AND ProfessorName = ?`, [courseName, professorsLastName], (err, result) => {
+                connection.release();
+                return err ? reject(err) : resolve(result);
+            });
         });
     });
 }
-
-connection.end();
 
 module.exports = {
     getISQData: getISQData,
